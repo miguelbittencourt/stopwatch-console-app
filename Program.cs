ed6992c5a28d09d83400f8f4bdf31a9b1b2bb34e
Miguel Bittencourt <miguelbittencourt45@gmail.com>
2023-01-25 17:58:57 -0200

    project upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+
+Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("S = segundo => 10s = 10 segundos");
+    Console.WriteLine("M = minuto => 1m = 1 minuto");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Quanto tempo deseja contar?");
+
+    string data = Console.ReadLine().ToLower();
+
+    if (data == "0")
+        System.Environment.Exit(0);
+    
+    char type = char.Parse(data.Substring(data.Length-1, 1));
+    int time = int.Parse(data.Substring(0, data.Length-1));
+    int multiplier = 1;
+
+    if (type == 'm')
+        multiplier = 60;
+
+    PreStart(time * multiplier);
+}
+
+static void PreStart(int time)
+{
+    Console.Clear();
+    Console.WriteLine("Ready...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Go...");
+    Thread.Sleep(1000);
+    Start(time);
+}
+
+static void Start(int time)
+{
+    int currentTime = 0;
+    while(currentTime != time)
+    {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+
+    Console.Clear();
+    Console.WriteLine("StopWatch finalizado");
+    Thread.Sleep(2000);
+    Menu();
+}
